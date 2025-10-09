dd14ba62cf60c782386861d701acbd5a5c9b3e66
thiolivs <thiolivs.s@gmail.com>
2025-10-08 22:21:36 -0300

    Create project

---
+++ types/global.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    namespace NodeJs{
+        interface ProcessEnv {
+            NEXT_PUBLIC_SUPABASE_URL : string;
+            NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+        }
+    }
+
+}
