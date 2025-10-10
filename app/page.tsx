6c0b2bbf5be5b01da5943023252f82d9e0059c6f
thiolivs <thiolivs.s@gmail.com>
2025-10-09 21:25:03 -0300

    Authentication with failure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,50 @@
-import Image from "next/image";
 
-export default function Home() {
-  return <div> HOME </div>
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
+import { CreateAccountForm } from "@/components/auth/create-account-form";
+import { LoginAccountForm } from "@/components/auth/login-account-form";
+
+export default async function Home() {
+  
+  let loggedIn = false
+
+  try{
+    const supabase = createServerComponentClient({ cookies });
+    const { data:{ session } } = await supabase.auth.getSession();
+    if (session) loggedIn = true
+  }
+  catch(error){
+    console.log("Home", error)
+  }
+
+  finally{
+    if (loggedIn) redirect("/user-app", RedirectType.replace);
+  }
+  
+return (
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <Tabs defaultValue="create-account" className="w-[400px] border rounded-md pb-4 shadow-2xl">
+        
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="create-account">Account</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="create-account">
+          <CreateAccountForm />
+        </TabsContent>
+
+        <TabsContent value="login">
+          <LoginAccountForm/>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
---
+++ app/user-app/page.tsx
@@ -0,0 +1,26 @@
+import { UserNav } from "@/components/common/user-nav";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+
+export default async function userApp(){
+
+    let loggedIn = false
+
+    try{
+    const supabase = createServerComponentClient({ cookies });
+    const { data:{ session } } = await supabase.auth.getSession();
+    if (session) loggedIn = true
+    }
+    catch(error){
+    console.log("UserApp", error)
+    }
+
+    finally{
+    if (!loggedIn) redirect("/", RedirectType.replace);
+    }
+
+return (
+<h1> <UserNav /> </h1>
+)
+}
