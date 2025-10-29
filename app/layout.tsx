3a88ce337a4c43a67bc398ff283f924bd5d98777
thiolivs <thiolivs.s@gmail.com>
2025-10-29 11:33:15 -0300

    fix: splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,44 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Caveat, Caveat_Brush } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const caveat = Caveat({
+  subsets: ["latin"],
+  weight: ["400", "700"], 
+  variable: "--font-caveat", 
+  display: "swap", 
+});
+
+const caveatBrush = Caveat_Brush({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-caveat-brush",
+  display: "swap",
+});
+
 
 export const metadata: Metadata = {
   title: "SeJoga",
-  description: " ",
+  description: "Seu app de boardgames",
   manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
     apple: "/sejoga-id/White192.png"
-  }
+  },
+  themeColor: "#DD2228"
 };
 
-export const viewport: Viewport = {
-  themeColor: "#51AFF7", 
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
