7a0ce31ca6693c66940b7cd6c2c3d30b6522ccaf
thiolivs <thiolivs.s@gmail.com>
2025-10-29 11:25:30 -0300

    fix: splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,61 +1,27 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Caveat, Caveat_Brush } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const caveat = Caveat({
-  subsets: ["latin"],
-  weight: ["400", "700"], 
-  variable: "--font-caveat", 
-  display: "swap", 
-});
-
-const caveatBrush = Caveat_Brush({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-caveat-brush",
-  display: "swap",
-});
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "SeJoga",
   description: " ",
-  manifest: "/manifest.json", 
-  themeColor: "#FFFFFF", 
-  appleWebApp: { 
-    capable: true,
-    statusBarStyle: "default",
-    title: "SeJoga",
-  },
-icons: { 
-    icon: [
-      { url: "/favicon.ico" }, // Favicon colorido para a aba do navegador
-    ],
-    apple: "/sejoga-id/IconWhite.png", // Ícone branco para iOS
-  },
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/sejoga-id/White192.png"
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#51AFF7", 
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${caveat.variable} ${caveatBrush.variable} antialiased`}
-      >
-          {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
