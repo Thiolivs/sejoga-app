47e70af0a31148e02ba4a897aa4ec1ac859a14f1
thiolivs <thiolivs.s@gmail.com>
2025-10-29 14:44:59 -0300

    fix: splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
-
+import { useState } from "react";
+import { SplashScreen } from "@/components/SplashScreen";
 
 export const metadata: Metadata = {
   title: "SeJoga",
@@ -8,21 +11,36 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
-    apple: "/sejoga-id/White192.png"
+    apple: "/sejoga-id/Meeple-192.png"
   }
 };
 
 export const viewport: Viewport = {
   themeColor: "#35588C",
 };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState(true);
+
   return (
     <html lang="pt-BR">
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#35588C" />
       </head>
-      <body>{children}</body>
+      <body>
+        {loading && (
+          <SplashScreen
+            onFinish={() => setLoading(false)}
+            logoUrl="/sejoga-id/White512.png" // 👈 usa o ícone branco aqui
+          />
+        )}
+        {!loading && children}
+      </body>
     </html>
   );
 }
