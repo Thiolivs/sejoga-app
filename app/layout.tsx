5156625d9e50b003d06bbe2cd059c5f0ad7a4fcc
thiolivs <thiolivs.s@gmail.com>
2025-11-20 20:35:19 -0300

    fix: status bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0096FF" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
       <body
         className={poppins.className}
