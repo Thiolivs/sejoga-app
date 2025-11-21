abb7a2d77c01e679f99838b34d3245a3fc557234
thiolivs <thiolivs.s@gmail.com>
2025-11-20 23:24:49 -0300

    fix: status bar

--- components/auth/login-account-form.tsx
+++ components/auth/login-account-form.tsx
@@ -30,7 +30,7 @@ const formSchema = z.object({
 });
 
 // ✅ Defina a versão aqui manualmente (sincronize com build.gradle)
-const APP_VERSION = "1.29";
+const APP_VERSION = "1.30";
 
 export function LoginAccountForm() {
     const router = useRouter();
