486954c7c33990bad68013563ad40dd55f02b967
thiolivs <thiolivs.s@gmail.com>
2025-11-20 21:39:33 -0300

    fix: version

--- components/auth/login-account-form.tsx
+++ components/auth/login-account-form.tsx
@@ -30,7 +30,7 @@ const formSchema = z.object({
 });
 
 // ✅ Defina a versão aqui manualmente (sincronize com build.gradle)
-const APP_VERSION = "1.22";
+const APP_VERSION = "1.23";
 
 export function LoginAccountForm() {
     const router = useRouter();
