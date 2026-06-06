8f773b010058bc498feb3f4f7173416ca9997b30
thiolivs <thiolivs.s@gmail.com>
2026-06-05 23:45:03 -0300

    test

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -39,11 +39,23 @@ export default function UserApp() {
     }, []);
 
     return (
-        <div className="flex flex-col h-screen overflow-hidden" style={{ overflow: 'hidden' }}>
+        <div className="flex flex-col h-screen overflow-hidden" style={{
+            height: '100vh',
+            maxHeight: '100vh',
+            overflow: 'hidden'
+        }}>
+            {debugInfo && (
+                <div className="fixed top-0 left-0 bg-red-500 text-white text-xs p-2 z-50 font-mono max-w-xs whitespace-pre">
+                    {debugInfo}
+                </div>
+            )}
 
+            <div className="flex-none">
+                <UserAppHeader />
+            </div>
 
             <div
-                className="flex-1 overflow-y-auto overflow-x-hidden min-h-0 border-4 border-red-500"
+                className="flex-1 overflow-y-auto overflow-x-hidden min-h-0"
                 style={{
                     height: '100%',
                     maxHeight: '100%'
