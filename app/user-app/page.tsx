3beb2bee9bb5caf9962225794ab641a0778fc41f
thiolivs <thiolivs.s@gmail.com>
2026-06-08 20:05:06 -0300

    test: firefox

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -49,6 +49,7 @@ function UserAppComponent() {
             </div>
 
             <div className="flex-none fixed bottom-0 left-0 right-0 z-50" style={{
+                paddingBottom: 'env(safe-area-inset-bottom)'
             }}>                <UserAppTabs
                     activeTab={activeTab}
                     onTabChange={handleTabChange}
